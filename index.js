55bd3e501f91e087968461674963f308ebad4ebb
Josh Duff <me@JoshDuff.com>
2015-11-23 10:13:57 -0600

    Updated to use the new renderer and noddity-render-static

--- index.js
+++ index.js
@@ -1,47 +1,37 @@
 var Ractive = require('ractive')
 var fs = require('fs')
-var Renderer = require('noddity-renderer')
+var render = require('noddity-render-static')
 
 Ractive.DEBUG = false
-var template = Ractive.parse(fs.readFileSync('post.html', { encoding: 'utf8' }))
+
+var templatePost = {
+	name: 'template',
+	metadata: {
+		title: 'RSS Template',
+		markdown: false
+	},
+	content: fs.readFileSync('post.html', { encoding: 'utf8' })
+}
 
 module.exports = function getStaticHtml(context, cb) {
 	if (typeof context.parameters.post === 'undefined') {
 		cb(new Error("You must provide the 'post' parameter"))
 	} else {
-		var butler = context.butler
-		var linkify = context.linkify
-		var renderer = new Renderer(butler, linkify)
-		var dumbResolve = context.resolvePost
 		var postFileName = context.parameters.post || 'index.md'
 
-		butler.getPosts(function(err, posts) {
+		var options = {
+			butler: context.butler,
+			linkifier: context.linkify,
+			data: {}
+		}
+
+		render(templatePost, postFileName, options, function(err, postHtml) {
 			if (err) {
 				cb(err)
 			} else {
-				butler.getPost(postFileName, function(err, post) {
-					renderer.renderPost(post, function (err, postHtml) {
-						if (err) {
-							cb(err)
-						} else {
-							posts.reverse()
-							posts.forEach(function(post) {
-								post.url = dumbResolve(post.filename)
-							})
-							var wholePageHtml = new Ractive({
-								el: '',
-								template: template,
-								data: {
-									postHtml: postHtml,
-									posts: posts,
-									title: post.metadata.title
-								}
-							}).toHTML()
-							cb(null, wholePageHtml)
-						}
-					})
-				})
+				cb(null, postHtml)
 			}
 		})
+
 	}
 }
--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 var noddityServer = require('noddity-service-server')
 var thisParticularImplementation = require('./index.js')
-noddityServer(thisParticularImplementation).listen(process.env.PORT)
+noddityServer(thisParticularImplementation).listen(process.env.PORT || 8888)
